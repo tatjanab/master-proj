30ecb6bc834b6c9db498e46c1574ea6e3cc7d039
Tatjana Ribic <tb@linkmedia.com>
2024-08-07 15:07:11 +0400

    updated the query, added error UI

--- src/hooks/useProductsByCategory.tsx
+++ src/hooks/useProductsByCategory.tsx
@@ -1,18 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { Product } from "../types/ProductType";
 
-function useProductsByCategory(categoryId: string) {
+function useProductsByCategory(categoryId: string | undefined) {
   // useEffect will be replaced with useQuery because of the easy error and loading handlers
-  const fetchProducts = async (): Promise<Product[]> => {
+  const fetchProducts = async (
+    categoryId: string | undefined,
+  ): Promise<Product[]> => {
     const response = await fetch(
       "https://master-shop-53976-default-rtdb.asia-southeast1.firebasedatabase.app/products.json",
     );
 
-    const data = await response.json();
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
 
-    const filteredProducts = data.filter(
+    const data = await response.json();
+    let filteredProducts: Product[] = data.filter(
       (product: Product) =>
-        product.category.toLowerCase() === categoryId.toLowerCase(),
+        product?.category.toLowerCase() === categoryId.toLowerCase(),
     );
 
     console.log("get category");
@@ -21,13 +26,17 @@ function useProductsByCategory(categoryId: string) {
   };
 
   // TODO:data will never be considered stale because of staleTime param, check if you change the title of the products
-  const { data: products, isLoading } = useQuery({
+  const {
+    data: products,
+    isLoading,
+    isError,
+  } = useQuery<Product[]>({
     queryKey: ["products", categoryId],
-    queryFn: fetchProducts,
+    queryFn: () => fetchProducts(categoryId),
     staleTime: Infinity,
   });
 
-  return { products, isLoading };
+  return { products, isLoading, isError };
 }
 
 export default useProductsByCategory;
