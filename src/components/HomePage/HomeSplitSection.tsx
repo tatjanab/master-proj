c5493fe4cac92b08be04ba759c38b2b6cc850cae
Tatjana Ribic <tb@linkmedia.com>
2024-09-05 09:48:59 +0400

    homepage responsiveness

--- src/components/HomePage/HomeSplitSection.tsx
+++ src/components/HomePage/HomeSplitSection.tsx
@@ -9,24 +9,28 @@ function HomeSplitSection() {
   };
 
   return (
-    <div className='split-section gap-x-10 mb-16 px-6'>
-      <div className='split-left flex-auto w-8/12'>
-        <div className='img-left'></div>
+    <div className='split-section flex md:flex-row flex-col gap-x-10 mb-16 px-6'>
+      <div className='split-left flex-auto md:w-8/12 w-full'>
+        <div className='img-left h-full'></div>
         <div className='section-title'>
-          <h2 className='underline uppercase mt-2 text-xl'>Bestsellers</h2>
+          <h2 className='underline uppercase mt-2 md:text-xl text-md'>
+            Bestsellers
+          </h2>
         </div>
       </div>
-      <div className='split-right flex-auto w-4/12'>
-        <div className='img-right'></div>
+      <div className='split-right flex-auto md:w-4/12 w-full'>
+        <div className='img-right md:h-2/3 h-full'></div>
         <div className='section-title'>
-          <h2 className='underline uppercase mt-2 text-xl'>Forever rugs</h2>
+          <h2 className='underline uppercase mt-2 md:text-xl text-md'>
+            Forever rugs
+          </h2>
         </div>
-        // passing data from child to parent using callback function
-        <Callback getColor={backgroundColor} />
+        {/* // passing data from child to parent using callback function */}
+        {/* <Callback getColor={backgroundColor} />
         <div
           className='p-5 h-2 w-2'
           style={{ backgroundColor: `${boxColor}` }}
-        ></div>
+        ></div> */}
       </div>
     </div>
   );
